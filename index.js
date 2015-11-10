aef0fc010100992569db5ba94c6d7c37d3b1c2f8
ForbesLindesay <forbes@lindesay.co.uk>
2015-11-10 01:30:22 +0000

    Update tests and add inputFormats

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var autoprefixer = require('autoprefixer');
 var postcss = require('jstransformer-postcss');
 
 exports.name = 'autoprefixer';
+exports.inputFormats = ['css'];
 exports.outputFormat = 'css';
 
 exports.renderAsync = function (str, options) {
